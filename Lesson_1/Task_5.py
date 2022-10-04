0dd419eda0efda21a089b178ae0e12dff4e8e417
Anastasia <Ambrella915@gmail.com>
2022-10-04 15:49:17 +0300

    [~] Доработала 5ю задачу из 1 урока

--- Lesson_1/Task_5.py
+++ Lesson_1/Task_5.py
@@ -1,19 +1,19 @@
 # Написать код, который выполняет пинг веб-ресурсов yandex.ru, youtube.com и преобразовывает результат из байтовового
 # типа данных в строковый без ошибок для любой кодировки операционной системы.
-
+import platform
 import subprocess
 
+from chardet import detect
 
-yandex = ['ping', 'yandex.ru']
-youtube = ['ping', 'youtube.com']
-subprocess_yandex = subprocess.Popen(yandex, stdout=subprocess.PIPE)
-subprocess_youtube = subprocess.Popen(youtube, stdout=subprocess.PIPE)
+def ping_url(domain):
+    ping = ['ping', domain]
 
-for line in subprocess_yandex.stdout:
-    line = line.decode('cp866').encode('utf-8')
-    print(line.decode('utf-8'), end='')
+    url_subprocess = subprocess.Popen(ping, stdout=subprocess.PIPE)
 
+    for line in url_subprocess.stdout:
+        result = detect(line)
+        encoded_line = line.decode(result['encoding']).encode(encoding='utf8')
+        print(encoded_line.decode(encoding='utf8'))
 
-for line in subprocess_youtube.stdout:
-    line = line.decode('cp866').encode('utf-8')
-    print(line.decode('utf-8'), end='')
+ping_url('yandex.ru')
+ping_url('youtube.com')
